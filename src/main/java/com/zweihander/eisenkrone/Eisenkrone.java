751ace26531564596247f2d4c38a377884a852b8
Gustaf Eggels <tencent.lj@gmail.com>
2026-07-02 22:10:27 +0300

    init: start project 0.1.0

---
+++ src/main/java/com/zweihander/eisenkrone/Eisenkrone.java
@@ -0,0 +1,42 @@
+package com.zweihander.eisenkrone;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(Eisenkrone.MODID)
+public class Eisenkrone {
+    public static final String MODID = "eisenkrone";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Eisenkrone(IEventBus modEventBus, ModContainer modContainer) {
+        // Регистрация своих блоков, предметов и т.д.
+    }
+}
---
+++ src/main/java/com/zweihander/eisenkrone/systems/Health.java
@@ -0,0 +1,4 @@
+package com.zweihander.eisenkrone.systems;
+
+public class Health {
+}
