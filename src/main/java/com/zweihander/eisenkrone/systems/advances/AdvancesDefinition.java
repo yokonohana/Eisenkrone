c455d1ddd36d13b962a220a1a080b655d369e0c9
Yokonohana <tencent.lj@gmail.com>
2026-07-21 12:22:38 +0300

    feat: add advances definition 0.2.20

--- src/main/java/com/zweihander/eisenkrone/systems/advances/AdvancesDefinition.java
+++ src/main/java/com/zweihander/eisenkrone/systems/advances/AdvancesDefinition.java
@@ -2,18 +2,21 @@ package com.zweihander.eisenkrone.systems.advances;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.zweihander.eisenkrone.systems.core.AttributeModifier;
+import com.zweihander.eisenkrone.systems.core.CodeX;
+import net.minecraft.resources.ResourceLocation;
 
 import java.util.List;
 
 public record AdvancesDefinition (
-        List<String> skills,
-        List<SkillBonus> bonus,
+        List<ResourceLocation> skills,
+        List<AttributeModifier> bonus,
         List<String> talents
 ) {
     public final static Codec<AdvancesDefinition> CODEC = RecordCodecBuilder.create(
             advancesDefinitionInstance -> advancesDefinitionInstance.group(
-                    Codec.STRING.listOf().optionalFieldOf("skills", List.of()).forGetter(AdvancesDefinition::skills),
-                    SkillBonus.CODEC.listOf().optionalFieldOf("bonus", List.of()).forGetter(AdvancesDefinition::bonus),
+                    CodeX.RESOURCE_LOCATION_CODEC.listOf().optionalFieldOf("skills", List.of()).forGetter(AdvancesDefinition::skills),
+                    AttributeModifier.CODEC.listOf().optionalFieldOf("bonus", List.of()).forGetter(AdvancesDefinition::bonus),
                     Codec.STRING.listOf().optionalFieldOf("talents", List.of()).forGetter(AdvancesDefinition::talents)
             ).apply(advancesDefinitionInstance, AdvancesDefinition::new));
 }
