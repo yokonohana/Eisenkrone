c1a08d581ebd734222166e01a5e27f958f7a882b
Yokonohana <tencent.lj@gmail.com>
2026-07-23 14:28:34 +0300

    feat: add ancestry json loader 0.3.26

--- src/main/java/com/zweihander/eisenkrone/systems/ancestry/AncestryJsonLoader.java
+++ src/main/java/com/zweihander/eisenkrone/systems/ancestry/AncestryJsonLoader.java
@@ -1,4 +1,43 @@
 package com.zweihander.eisenkrone.systems.ancestry;
 
-public class AncestryJsonLoader {
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.mojang.logging.LogUtils;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.JsonOps;
+import com.zweihander.eisenkrone.Eisenkrone;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class AncestryJsonLoader extends SimpleJsonResourceReloadListener {
+
+    public AncestryJsonLoader() {
+        super(new Gson(), "ancestry");
+    }
+
+    @Override
+    protected void apply(@NotNull Map<ResourceLocation, JsonElement> resourceLocationJsonElementMap,
+                         @NotNull ResourceManager resourceManager, @NotNull ProfilerFiller profilerFiller) {
+        AncestryRegistry.clear();
+
+        for (Map.Entry<ResourceLocation, JsonElement> entry : resourceLocationJsonElementMap.entrySet()) {
+            ResourceLocation id = entry.getKey();
+            JsonElement value = entry.getValue();
+
+            DataResult<AncestryDefinition> searchResult = AncestryDefinition.CODEC.parse(JsonOps.INSTANCE, value);
+            Optional<AncestryDefinition> mbAncestry = searchResult.resultOrPartial(err -> System.err.println(
+                    err + " from file: " + id
+            ));
+
+            mbAncestry.ifPresent(ancestry -> AncestryRegistry.registry(id, ancestry));
+        }
+
+        LogUtils.getLogger().info("Loaded {} ancestry", AncestryRegistry.getAll().size());
+    }
 }
