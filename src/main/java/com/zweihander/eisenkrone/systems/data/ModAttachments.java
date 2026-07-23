b128f8173413e0016de5347521d1f2f6442012e4
Yokonohana <tencent.lj@gmail.com>
2026-07-23 14:30:47 +0300

    feat: add mod attachmaents 0.4.30

--- src/main/java/com/zweihander/eisenkrone/systems/data/ModAttachments.java
+++ src/main/java/com/zweihander/eisenkrone/systems/data/ModAttachments.java
@@ -1,4 +1,17 @@
 package com.zweihander.eisenkrone.systems.data;
 
-public class ModAttachment {
+import com.zweihander.eisenkrone.Eisenkrone;
+import com.zweihander.eisenkrone.systems.data.attributes.AttributeContainer;
+import net.neoforged.neoforge.attachment.AttachmentType;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+import java.util.function.Supplier;
+
+public class ModAttachments {
+    public static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES =
+            DeferredRegister.create(NeoForgeRegistries.Keys.ATTACHMENT_TYPES, Eisenkrone.MODID);
+
+    public static final Supplier<AttachmentType<AttributeContainer>> ATTRIBUTES = ATTACHMENT_TYPES.register("attributes",
+            () -> AttachmentType.builder(AttributeContainer::new).serialize(AttributeContainer.CODEC).build());
 }
