886c587fab3597927195ac34a0de498720a41703
Yokonohana <tencent.lj@gmail.com>
2026-07-23 14:29:21 +0300

    feat: add size type 0.3.28

--- src/main/java/com/zweihander/eisenkrone/systems/ancestry/SizeType.java
+++ src/main/java/com/zweihander/eisenkrone/systems/ancestry/SizeType.java
@@ -1,4 +1,25 @@
 package com.zweihander.eisenkrone.systems.ancestry;
 
-public enum SizeType {
+import com.mojang.serialization.Codec;
+import net.minecraft.util.StringRepresentable;
+import org.jetbrains.annotations.NotNull;
+
+public enum SizeType implements StringRepresentable {
+    SMALL("small"),
+    NORMAL("normal"),
+    LARGE("large"),
+    HUGE("huge");
+
+    public static final Codec<SizeType> CODEC = StringRepresentable.fromEnum(SizeType::values);
+
+    private final String serializedName;
+
+    SizeType(String serializedName) {
+        this.serializedName = serializedName;
+    }
+
+    @Override
+    public @NotNull String getSerializedName() {
+        return serializedName;
+    }
 }
