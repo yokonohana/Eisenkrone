5a7dfb5b106ff89d45d45f1b48775be0ba36d9be
Yokonohana <tencent.lj@gmail.com>
2026-07-23 14:28:58 +0300

    feat: add ancestry json registry 0.3.27

--- src/main/java/com/zweihander/eisenkrone/systems/ancestry/AncestryRegistry.java
+++ src/main/java/com/zweihander/eisenkrone/systems/ancestry/AncestryRegistry.java
@@ -1,4 +1,29 @@
 package com.zweihander.eisenkrone.systems.ancestry;
 
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+
 public class AncestryRegistry {
+    private final static LinkedHashMap<ResourceLocation, AncestryDefinition> store = new LinkedHashMap<>();
+
+    private AncestryRegistry() {}
+
+    public static void registry(ResourceLocation id, AncestryDefinition ancestry) {
+        store.put(id, ancestry);
+    }
+
+    public static AncestryDefinition getById(ResourceLocation id) {
+        return store.get(id);
+    }
+
+    public static Collection<AncestryDefinition> getAll() {
+        return Collections.unmodifiableCollection(store.values());
+    }
+
+    public static void clear() {
+        store.clear();
+    }
 }
